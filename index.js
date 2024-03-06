866762e72bb9e4aa6de03d607f1e21220ef60d31
Woosterwebdev <136399860+Woosterwebdev@users.noreply.github.com>
2024-03-06 14:56:37 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -27,3 +27,77 @@ const posts = [
         likes: 152
     }
 ]
+
+const sectionEl = document.getElementById("section-el")
+
+// render posts from object
+
+renderPosts()
+
+function renderPosts() {
+    
+    clearSectionEl()
+
+    for (let i = 0; i < posts.length; i++) {
+        const name = posts[i].name
+        const username = posts[i].username
+        const location = posts[i].location
+        const avatar = posts[i].avatar
+        const post = posts[i].post
+        const comment = posts[i].comment
+        const likes = posts[i].likes
+
+        appendPost(name, username, location, avatar, post, comment, likes)
+    }
+    
+}
+
+// Clear innerHTML of Section Element
+
+function clearSectionEl() {
+    sectionEl.innerHTML = ""
+}
+
+// append post
+
+function appendPost(name, username, location, avatar, post, comment, likes) {
+    sectionEl.innerHTML += `
+    <div class="container">
+              <div class="container post-head">
+                <img src="${avatar}" class="avatar" />
+                <div class="name-loaction">
+                  <p>
+                    <span class="name bold">${name}</span>${location}
+                  </p>
+                </div>
+              </div>
+              <img class="post-img" src=${post} />
+              <div class="post-body m-10">
+                <div>
+                  <img class="icon" src="images/icon-heart.png" />
+                  <img class="icon" src="images/icon-comment.png" />
+                  <img class="icon" src="images/icon-dm.png" />
+                </div>
+                <p class="bold">${likes} likes</p>
+                <p class="user-caption">
+                  <span class="bold">${username}</span> ${comment}
+                </p>
+              </div>
+            </div>`
+}
+
+// Sticky Header
+
+// window.onscroll = function() {stickyHeader()}
+
+// const headerEl = document.getElementById("header")
+
+// const sticky = headerEl.offsetTop
+
+// function stickyHeader() {
+//     if (window.pageYOffset >= sticky) {
+//         headerEl.classList.add("sticky")
+//     } else {
+//         headerEl.classList.remove("sticky")
+//     }
+// }
